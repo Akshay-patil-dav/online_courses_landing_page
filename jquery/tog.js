3898f0b9b373f53aabd2b8fed787027275e3a7bc
Akshay Patil <akshaypatil22082002@gmail.com>
2024-11-25 15:00:12 +0530

    Add files via upload

--- jquery/tog.js
+++ jquery/tog.js
@@ -4,14 +4,33 @@ $(document).ready(function () {
     });
 });
 
-
-// ******************************************
 $(document).ready(function () {
     // Toggle visibility on hover
     $('.toggle-button').hover(
         function () {
             // Show the menu
             $('.men-tog').stop(true, true).toggle();
+            $('.men-tog1').hide()
+        },
+        function () {
+            // Optionally: do nothing here if you want the toggle to persist
+        }
+    );
+
+    // Optional: ensure the menu toggles back when hovering out
+ 
+});
+
+
+
+$(document).ready(function () {
+    // Toggle visibility on hover
+    $('.toggle-button1').hover(
+        function () {
+            // Show the menu
+            $('.men-tog1').stop(true, true).toggle();
+            $('.men-tog').stop(true, true).toggle();
+            $('.men-tog').hide()
         },
         function () {
             // Optionally: do nothing here if you want the toggle to persist
